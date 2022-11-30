1c01daf4ed09f49a8e3574ed2ea8316f1a2ac5dc
Tianyu Wei <billywty@caen-vnc-mi03.engin.umich.edu>
2022-11-30 16:33:31 -0500

    pe_row synthesized

--- rtl/utils/shift_reg.sv
+++ rtl/utils/shift_reg.sv
@@ -16,24 +16,44 @@ module shift_reg #(
 );
     // group in this order to facilitate shifting.
     logic [DATA_WIDTH-1:0][DEPTH-1:0] mem;
+    logic [DEPTH-1:0] used;
+    logic [DATA_WIDTH-1:0][DEPTH-1:0] shift_mem;
+    logic [DATA_WIDTH-1:0][DEPTH-1:0] next_mem;
 
-    genvar i;
+    genvar i, j;
     generate
         for(i = 0; i < DATA_WIDTH; i++) begin
-            assign o_rdata[i] = mem[i][i_used_depth - 1];
+            assign o_rdata[i] = mem[i][0];
+            
+            assign shift_mem[i]  = {mem[i][0], mem[i][DEPTH-1:1]};
+
+            for(j = 0; j < DEPTH; j++) begin
+                always_comb begin
+                    if(i_ren)
+                        next_mem[i][j] = used[j] ? shift_mem[i][j] : mem[i][0];
+                    else if(i_wen)
+                        next_mem[i][j] = (j==i_used_depth-1) ? i_wdata[i] :
+                                         (used[j] ? shift_mem[i] : 0);
+                    else
+                        next_mem[i][j] = mem[i][j];
+                end
+            end
 
             // synopsys sync_set_reset "reset"
             always_ff @(posedge clk) begin
                 if(reset) begin
                     mem[i] <= 0;
                 end
-                else if(i_ren) begin
-                    mem[i] <= {mem[i][DEPTH-2:0], mem[i][i_used_depth - 1]};
-                end
-                else if(i_wen) begin
-                    mem[i] <= {mem[i][DEPTH-2:0], o_rdata[i]};
+                else begin
+                    mem[i] <= next_mem[i];
                 end
             end
+        end
+    endgenerate
+
+    generate
+        for(i = 0; i < DEPTH; i++) begin
+            assign used[i] = (i < i_used_depth);
         end
     endgenerate
 
